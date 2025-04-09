804ea643d2290be76ee7c68692812b9de6920fc0
Theo Mousnier <mousniert.pro@gmail.com>
2025-04-09 16:26:26 +0200

    Initial commit

---
+++ src/main/java/com/example/infoevenement/InfoEvenementApplication.java
@@ -0,0 +1,13 @@
+package com.example.infoevenement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoEvenementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfoEvenementApplication.class, args);
+    }
+
+}
