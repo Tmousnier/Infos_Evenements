f0bbb3e3db9d4e1157f69712f6095c9119a9eb70
Theo Mousnier <mousniert.pro@gmail.com>
2025-04-10 12:22:51 +0200

    update name mapper

--- src/main/java/com/example/infoevenement/Mapper/EvenementMapper.java
+++ src/main/java/com/example/infoevenement/Mapper/EvenementMapper.java
@@ -1,24 +1,16 @@
-package com.example.infoevenement.Mapper;
+package com.example.infoevenement.mapper;
 
 import com.example.infoevenement.dao.Evenement;
 import com.example.infoevenement.dto.EvenementDto;
 import com.example.infoevenement.dto.EvenementInput;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+
 
 @Mapper(componentModel = "spring")
 public interface EvenementMapper {
-
-        @Mapping(source = "libelle", target = "libelle")
-        @Mapping(source = "description", target = "description")
-        @Mapping(source = "lieux", target = "lieux")
         EvenementDto toDto(Evenement evenement);
 
-        @Mapping(source = "libelle", target = "libelle")
-        @Mapping(source = "description", target = "description")
-        @Mapping(source = "lieux", target = "lieux")
-        @Mapping(target = "id", ignore = true)
         Evenement toEntity(EvenementInput evenementInput);
-    }
+}
 
 
