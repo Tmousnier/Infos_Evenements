cad47a2d111cc482e7f2b65c118c9ceacc52b229
Staxiii <fouquetalexis@hotmail.com>
2025-04-10 14:22:09 +0200

    Add migration Event table

--- src/main/resources/db.migration/V1__Create_Evenement.sql
+++ src/main/resources/db.migration/V1__Create_Evenement.sql
@@ -2,17 +2,20 @@ CREATE TABLE IF NOT EXISTS Evenement  (
                            id VARCHAR(100) PRIMARY KEY,
                            libelle VARCHAR(255) NOT NULL,
                            description TEXT,
-                           lieu_id VARCHAR(100)
+                           category_id VARCHAR(100),
+                           periode_id VARCHAR(100),
+                           lieux_id VARCHAR(100),
+
+    CONSTRAINT fk_evenement_categorie FOREIGN KEY (category_id)
+    REFERENCES category(id),
+
+    CONSTRAINT fk_evenement_periode FOREIGN KEY (periode_id)
+    REFERENCES periode(id),
+
+    CONSTRAINT fk_evenement_lieu FOREIGN KEY (lieux_id)
+    REFERENCES lieux(id)
+
+
 );
 
-/* Liaisons
 
-CONSTRAINT fk_evenement_categorie FOREIGN KEY (categorie_id)
-    REFERENCES Categorie(id),
-
-CONSTRAINT fk_evenement_periode FOREIGN KEY (periode_id)
-    REFERENCES Periode(id),
-
-CONSTRAINT fk_evenement_lieu FOREIGN KEY (lieu_id)
-    REFERENCES Lieu(id)
-   */
