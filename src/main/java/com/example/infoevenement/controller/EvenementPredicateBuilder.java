58e90833946b8d4aa0c3a78d37d8a213931081a2
Theo Mousnier <mousniert.pro@gmail.com>
2025-04-11 11:02:19 +0200

    add category and periode EvenementPredicateBuilder

--- src/main/java/com/example/infoevenement/controller/EvenementPredicateBuilder.java
+++ src/main/java/com/example/infoevenement/controller/EvenementPredicateBuilder.java
@@ -4,7 +4,7 @@ import com.example.infoevenement.dao.QEvenement;
 import com.querydsl.core.types.dsl.BooleanExpression;
 
 public class EvenementPredicateBuilder {
-    public BooleanExpression build(String libelleContain, String lieux) {
+    public BooleanExpression build(String libelleContain, String lieux, String category, String periode) {
         BooleanExpression predicate = QEvenement.evenement.isNotNull();
 
         if (libelleContain != null && !libelleContain.isEmpty()) {
@@ -12,6 +12,12 @@ public class EvenementPredicateBuilder {
         }
         if (lieux != null && !lieux.isEmpty()) {
             predicate = predicate.and(QEvenement.evenement.lieux.id.eq(lieux));
+        }
+        if (category != null && !category.isEmpty()) {
+            predicate = predicate.and(QEvenement.evenement.category.id.eq(category));
+        }
+        if (periode != null && !periode.isEmpty()) {
+            predicate = predicate.and(QEvenement.evenement.periode.id.eq(periode));
         }
 
         return predicate;
